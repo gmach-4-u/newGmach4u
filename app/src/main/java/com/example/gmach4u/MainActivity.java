afe335ec969b496e3eeca87a5469024ff6d73d7f
chenkatzo@gmail.com <chenkatzo@gmail.com>
2020-11-30 23:28:52 +0200

    DataBase first try

--- app/src/main/java/com/example/gmach4u/MainActivity.java
+++ app/src/main/java/com/example/gmach4u/MainActivity.java
@@ -6,10 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 Button login;
 Button register;
+FirebaseDatabase mDatabase ;
+DatabaseReference dbRootRef;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +47,8 @@ Button register;
         login.setOnClickListener(this);
       //  button2.setOnClickListener(this);
         register.setOnClickListener(this);
+
+        mDatabase = FirebaseDatabase.getInstance();
     }
 
     @Override
--- app/src/main/java/com/example/gmach4u/RegisterActivity.java
+++ app/src/main/java/com/example/gmach4u/RegisterActivity.java
@@ -2,13 +2,65 @@ package com.example.gmach4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    Button submit;
+    TextView phone, password, name, email;
+
+    FirebaseDatabase RootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        submit = (Button) findViewById(R.id.submit);
+        phone = (TextView)findViewById(R.id.phone);
+        email = (TextView) findViewById(R.id.email);
+        password = (TextView) findViewById(R.id.password);
+        name = (TextView)findViewById(R.id.name);
+
+        submit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==submit) {
+            RootNode = FirebaseDatabase.getInstance();
+            reference = RootNode.getReference("Users");
+
+
+            String Name = name.getText().toString();
+            String Email = email.getText().toString();
+            String Phone = phone.getText().toString();
+            String Password = password.getText().toString();
+
+
+
+            userHelper user = new userHelper(Name, Password, Email, Phone);
+            user.phone=reference.push().getKey();
+
+            user.setEmail(Email);
+            user.setName(Name);
+            user.setPassword(Password);
+            user.setPhone(Phone);
+
+            reference.child(Phone).setValue(user);
+
+            Intent intent = new Intent(this,loginActivity.class);
+            startActivity(intent);
+        }
     }
 }
