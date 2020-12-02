b5028e63ee419b87b9fc7f429a98df47df68f631
chenkazover <chenkatzo@gmail.com>
2020-12-02 20:17:39 +0200

    add all the activities

---
+++ app/src/main/java/com/example/gmach4u/GmachStockClient.java
@@ -0,0 +1,14 @@
+package com.example.gmach4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GmachStockClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gmach_stock_client);
+    }
+}
