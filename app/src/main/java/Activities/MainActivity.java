341640f1180368bd31d03406c50d18cdb65d47ee
noayair <58184656+noayair@users.noreply.github.com>
2020-12-06 16:48:14 +0200

    Noa2

--- app/src/main/java/Activities/MainActivity.java
+++ app/src/main/java/Activities/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private Button Logout;
     FirebaseAuth firebaseAuth;
     //Noa
+    //Noa2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
